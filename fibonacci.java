cded2ddab6d6d4f4aa0f4c6d98040ba182499da7
NikBirajdar <99384194+NikBirajdar@users.noreply.github.com>
2022-02-10 12:55:45 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,30 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class fibonacci
+{
+	public static void main(String args[])
+	{
+		int n1=0,n2=1,n3,count;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the count..");
+		count= s.nextInt();
+
+		System.out.print(n1+" "+n2);   //printing 0 and 1    
+		for(int i=2;i<count;i++)
+		{
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+
+			}
+
+
+	}		
+
+
+
+
+}
